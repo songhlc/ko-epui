fa173aa6e75cb1bd9c5c0a3d2ca972c11f9044c9
liuxu7 <liuxu7@ufsoft.com.cn>
2017-02-27 16:33:26 +0800

    v1.4.3 新增u-pagination

--- src/u-pagination/index.js
+++ src/u-pagination/index.js
@@ -1,7 +1,7 @@
 import {CreatApp} from '../base'
 function createDropdown (params, componentInfo) {
   let umeta = {'type': 'pagination'}
-  console.log(params)
+  // console.log(params)
   CreatApp(componentInfo, umeta, params)
 }
 export default createDropdown
