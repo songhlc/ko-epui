bd757df0c57dbdeb4a6a470a3d5bef4013845961
liuxu7 <liuxu7@ufsoft.com.cn>
2017-02-27 13:49:43 +0800

    2.27_lx_分页

---
+++ src/u-pagination/index.js
@@ -0,0 +1,6 @@
+import {CreatApp} from '../base'
+function createDropdown (params, componentInfo) {
+  let umeta = {'type': 'pagination'}
+  CreatApp(componentInfo, umeta, params)
+}
+export default createDropdown
