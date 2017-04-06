4253b574a57fc20e7dff9c94aa33ff0ef0df3017
windknow <songhlc@yonyou.com>
2017-04-06 18:48:59 +0800

    添加submit组件

--- src/components/u-select/index.js
+++ src/components/u-select/index.js
@@ -2,6 +2,8 @@
 import {CreatApp} from '../base'
 function createDropdown (params, componentInfo) {
   let umeta = {'type': 'u-combobox'}
+  let dom = componentInfo.element.children[0]
+  dom.
   CreatApp(componentInfo, umeta, params)
 }
 export default createDropdown
