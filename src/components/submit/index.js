7da9c62ff51d626433c8b43db2df90a6829191b2
windknow <songhlc@yonyou.com>
2017-06-07 16:18:19 +0800

    重新编译

--- src/components/submit/index.js
+++ src/components/submit/index.js
@@ -1,15 +1,15 @@
 /* global ko */
-import {debounce} from 'lodash'
+import _ from 'lodash'
 function init (params) {
+  let flag = true
   this.text = ko.observable(params.text || '')
   this.click = _.debounce(function () {
     // 加入4s内防止再次点击机制，一旦点击发送click事件则4s内不再提交
     if (flag) {
       params.click()
       flag = false
-      var timeout = setTimeout(function () {
-        flag = true;
-        timeout = null;
+      setTimeout(function () {
+        flag = true
       }, 4000)
     }
   }, params.debounce || 300, { 'maxWait': params.maxWait || 2000 })
